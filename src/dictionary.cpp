ad65d03e1d9f5eea520c23cf491b49ccb149adc4
John Bonforte <johnbonforte@gmail.com>
2012-05-31 12:18:09 -0300

    Added Dictionary class and modified main to use it
    The Dictionary class contains:
     - bool Check (string) -> check if the word is valid
     - Dictionary constructor
    
    Private elements:
     - std::set < string >
    
    main.cpp now reads input from the user stdin and checks if the word is
    valid

---
+++ src/dictionary.cpp
@@ -0,0 +1,11 @@
+#include "dictionary"
+
+Dictionary::Dictionary(const std::string & fname) 
+{
+    
+}
+
+bool Dictionary::Check(const std::string & word) const
+{
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
+#include "dictionary.h"
 using namespace std;
 int main(int argc, char ** argv)
 {
             std::cout << "scheck version 0.1" << std::endl;
+            Dictionary d( "data/mydict.dat" );
+            string word = "dog";
+            while ( getline(cin, word ) ) {
+                if ( d.Check( word ) ) {
+                    cout << word << " is OK\n";
+                }
+                else {
+                    cout << word << " is misspelt \n";
+                }
+            }
 }
 
